b62b9be0297d08c728b2bc87e6fb4f065f8e1412
unknown <git@p1at.dev>
2022-12-15 02:10:42 +0900

    chore: format setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import { invoke } from "@tauri-apps/api/tauri"
 import Image from "next/image"
 
 const Page = () => {
-    return (
-        <div className="p-8">
-            <p className="text-8xl">Welcome to Tauri!</p>
-        </div>
-    )
+  return (
+    <div className="p-2 h-full bg-red w-full">
+      <div className="flex">
+        <div className=""></div>
+      </div>
+    </div>
+  )
 }
 
 export default Page
