37debbfb55c9752c2782fe5c65fdfd47c44badbe
unknown <git@p1at.dev>
2022-12-15 19:57:21 +0900

    feat: window config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,6 +11,19 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            tauri::WindowBuilder::new(
+                app,
+                "portal_wizard", // the unique label
+                tauri::WindowUrl::App("index.html".into()),
+            )
+            .title("Portal Wizard")
+            .resizable(true)
+            .min_inner_size(640.0, 640.0)
+            .build()
+            .expect("failed to create window");
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
