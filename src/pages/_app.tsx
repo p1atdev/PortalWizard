f45ce2c74c3c3405450edcff1c58dbe19357c1aa
unknown <git@p1at.dev>
2022-12-15 19:47:57 +0900

    chore: switched to vite

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from "next/app"
-import { useEffect } from "react"
-import "../global.css"
-
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
-
-export default App
